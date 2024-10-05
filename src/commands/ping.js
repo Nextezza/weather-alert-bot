17b9b3a1e6763bd9ea9e2b0e521d6105532575a1
GodKode69 <157723750+GodKode69@users.noreply.github.com>
2024-10-05 14:24:33 +0530

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  name: "ping",
+  aliases: [`pong`, `speed`],
+  owner: false,
+  run: async (client, message, args) => {
+    message.channel.send(`🏓 Pong! ${client.ws.ping}ms`);
+  },
+};
