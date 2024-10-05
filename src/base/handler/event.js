2786273d59d66171a0da537d213058e5e57f0510
GodKode69 <157723750+GodKode69@users.noreply.github.com>
2024-10-05 13:41:59 +0530

    Update event.js

--- src/base/handler/event.js
+++ src/base/handler/event.js
@@ -1,1 +1,24 @@
+const { readdirSync } = require("fs");
 
+const loadEvents = (client) => {
+  let eventCount = 0;
+
+  readdirSync("./events/").forEach((dir) => {
+    const eventFiles = readdirSync(`./events/${dir}/`).filter((file) =>
+      file.endsWith(".js")
+    );
+    for (const file of eventFiles) {
+      try {
+        const event = require(`../../events/${dir}/${file}`);
+        event(client);
+        eventCount++;
+      } catch (error) {
+        console.error(`Error loading event ${file}: ${error.message}`);
+      }
+    }
+  });
+
+  return { eventCount };
+};
+
+module.exports = { loadEvents };
