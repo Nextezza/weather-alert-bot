24c39a2e4c50e2818095e0d9566c9684f41462be
GodKode69 <157723750+GodKode69@users.noreply.github.com>
2024-10-05 13:41:29 +0530

    Update command.js

--- src/base/handler/command.js
+++ src/base/handler/command.js
@@ -1,1 +1,30 @@
+const { readdirSync, statSync } = require("fs");
+const path = require("path");
 
+const loadCommands = (client) => {
+  let commandCount = 0;
+
+  const readCommands = (directory) => {
+    const filesAndDirs = readdirSync(directory);
+
+    for (const item of filesAndDirs) {
+      const fullPath = path.join(directory, item);
+      const stat = statSync(fullPath);
+
+      if (stat.isDirectory()) {
+        readCommands(fullPath);
+      } else if (item.endsWith(".js")) {
+        const cmd = require(fullPath);
+        client.commands.set(cmd.name, cmd);
+        commandCount++;
+      }
+    }
+  };
+
+  const commandsPath = path.join(__dirname, "../../commands");
+  readCommands(commandsPath);
+
+  return { commandCount };
+};
+
+module.exports = { loadCommands };
